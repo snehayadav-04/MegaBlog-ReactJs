68baf2cae7133fd8cbe8034228b947f0d6502009
Sneha Yadav <126853144+snehayadav-04@users.noreply.github.com>
2025-09-18 10:02:31 +0000

    environment variable and authentication

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 
 function App() {
-  
+  console.log(import.meta.env.VITE_APPWRITE_URL);
   return (
     <>
       <h1>Mega Blog </h1>
---
+++ MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,19 @@
+import conf from '../config.js';
+import {Client, Account, ID} from 'appwrite';
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor(){
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+            this.account = new Account(this.client);
+    }
+}
+
+
+const authService = new AuthService();
+
+export default AuthService;
